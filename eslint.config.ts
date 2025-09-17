984ced9335ee384dc41a02b12fd0e92fdf638401
Jerry Bruwes <jbruwes@icloud.com>
2025-09-17 21:02:16 +0200

    new file:   eslint.config.ts

---
+++ eslint.config.ts
@@ -0,0 +1,41 @@
+import eslint from "@eslint/js";
+import { flatConfigs } from "eslint-plugin-import-x";
+import perfectionist from "eslint-plugin-perfectionist";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import { defineConfig } from "eslint/config";
+import { configs } from "typescript-eslint";
+
+export default defineConfig(
+  { ignores: ["**/dist"] },
+  {
+    rules: {
+      "@typescript-eslint/no-use-before-define": ["error", "nofunc"],
+      "import-x/no-extraneous-dependencies": [
+        "error",
+        {
+          devDependencies: ["**/eslint.config.ts", "**/vite.config.ts"],
+          optionalDependencies: false,
+        },
+      ],
+      "no-use-before-define": "off",
+    },
+  },
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: {
+          allowDefaultProject: ["eslint.config.ts", "vite.config.ts"],
+        },
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  eslint.configs.recommended,
+  //@ts-expect-error Argument of type 'PluginFlatConfig' is not assignable to parameter of type 'InfiniteArray<ConfigWithExtends>'.
+  flatConfigs.recommended,
+  flatConfigs.typescript,
+  configs.strictTypeChecked,
+  configs.stylisticTypeChecked,
+  perfectionist.configs["recommended-natural"],
+  eslintPluginPrettierRecommended,
+);
