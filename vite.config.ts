0c795d60398dd54af628b12dd13925c99824698c
Jerry Bruwes <jbruwes@icloud.com>
2025-09-17 21:03:03 +0200

    new file:   vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import { resolve } from "node:path";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  build: {
+    emptyOutDir: false,
+    lib: {
+      entry: resolve(__dirname, "src/vue.worker.ts"),
+      fileName: "vue.worker",
+      formats: ["es"],
+    },
+  },
+});
