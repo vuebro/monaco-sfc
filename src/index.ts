52c36485c5a58b12947e98c8e95e2a49a861d8a3
Jerry Bruwes <jbruwes@icloud.com>
2025-09-18 17:36:50 +0200

    modified:   src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import type { WorkerLanguageService } from "@volar/monaco/worker";
-import type Monaco from "monaco-editor";
+import type Monaco from "monaco-editor-core";
 
 import {
   activateAutoInsertion,
@@ -9,25 +9,37 @@ import {
 
 import * as languageConfigs from "./language-configs";
 
-export function configureMonacoTailwindcss(
-  monaco: typeof import("monaco-editor"),
+export function configureMonacoSFC(
+  monaco: typeof import("monaco-editor-core"),
 ) {
   const getSyncUris = () => monaco.editor.getModels().map(({ uri }) => uri),
     id = "vue",
+    languageId: string[] = [],
     worker: Monaco.editor.MonacoWebWorker<WorkerLanguageService> =
       monaco.editor.createWebWorker({
         label: id,
         moduleId: "vs/language/vue/vueWorker",
       });
 
-  monaco.languages.register({
-    aliases: [id],
-    extensions: [`.${id}`],
-    id,
-  });
-  monaco.languages.setLanguageConfiguration(id, languageConfigs.vue);
+  monaco.languages.register({ aliases: [id], extensions: [`.${id}`], id });
 
-  void registerProviders(worker, [id], getSyncUris, monaco.languages);
-  activateMarkers(worker, [id], id, getSyncUris, monaco.editor);
-  activateAutoInsertion(worker, [id], getSyncUris, monaco.editor);
+  const languages = monaco.languages.getLanguages();
+
+  Object.entries(languageConfigs).forEach(
+    ([alias, configuration]: [
+      string,
+      Monaco.languages.LanguageConfiguration,
+    ]) => {
+      const { id } =
+        languages.find(({ aliases }) => aliases?.includes(alias)) ?? {};
+      if (id) {
+        languageId.push(id);
+        monaco.languages.setLanguageConfiguration(id, configuration);
+      }
+    },
+  );
+
+  void registerProviders(worker, languageId, getSyncUris, monaco.languages);
+  activateMarkers(worker, languageId, id, getSyncUris, monaco.editor);
+  activateAutoInsertion(worker, languageId, getSyncUris, monaco.editor);
 }
