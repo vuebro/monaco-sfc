86ff9341afa30210985895572bd467d00307b996
Jerry Bruwes <jbruwes@icloud.com>
2025-10-21 17:58:59 +0200

    refactor: update ESLint config and project settings

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,17 +1,17 @@
 import shared from "@vuebro/configs/eslint";
 import { defineConfig } from "eslint/config";
 
-export default defineConfig(shared, {
-  languageOptions: {
-    parserOptions: {
-      projectService: {
-        allowDefaultProject: [
-          "eslint.config.ts",
-          "vite.config.ts",
-          "prettierrc.config.ts",
-        ],
+export default defineConfig(
+  shared,
+  { ignores: ["src/language-configs.ts"] },
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: {
+          allowDefaultProject: ["eslint.config.ts", "vite.config.ts"],
+        },
+        tsconfigRootDir: import.meta.dirname,
       },
-      tsconfigRootDir: import.meta.dirname,
     },
   },
-});
+);
